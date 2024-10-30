c0d9b2c1e34a57c262eb57fe99df02ecc06fa9af
westonmeacham <weston.meacham@gmail.com>
2024-10-30 15:28:12 -0600

    Adding all of my code to the project

---
+++ flash-card/js/script.js
@@ -0,0 +1,27 @@
+const term = document.querySelector('.term');
+const definition = document.querySelector('.definition');
+const checkButton = document.querySelector('.check');
+const nextButton = document.querySelector('.next');
+
+let words = {
+    Hello: "Iakwe",
+    Goodbye: "Jerramon",
+    "I love you": "Ij Iakwe Eok"
+}
+
+let data = object.entries(words)
+
+function getRandomTerm() {
+    randomTerm = data[Math.floor(Math.random() * data.length)]
+    console.log(randomTerm);
+}
+
+checkButton.addEventListener('click', function() {
+    definition.computedStyleMap.display = 'block';
+});
+
+nextButton.addEventListener('click', function() {
+    console.log("You clicked the next button")
+});
+
+getRandomTerm()
